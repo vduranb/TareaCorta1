274725adc421292ad648b991c752ebe4ee336b87
vduranb <37748410+vduranb@users.noreply.github.com>
2018-08-27 14:58:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,21 +28,23 @@ class lista{
 private:
     pnodo primero;
     pnodo actual;
-    pnodo Cola[5];
+    pnodo Cola[5];  //Cola de punteros a nodos
 
 public:
     lista(){primero = actual = NULL;}
-    void InsertarFinal(string v);
-    void Mostrar();
-    void lectura(string arch);
+    void InsertarFinal(string v, int i);
+    void Mostrar(int i);
+    void lectura(string arch, int i);
     bool ListaVacia() { return primero == NULL; }
+    void cola();
 
 };
 
-void lista::InsertarFinal(string v)
+// El i refiere a la posicion de la cola
+void lista::InsertarFinal(string v,int i)   
 {
    if (ListaVacia())
-     primero = new nodo(v);
+     Cola[i] =primero = new nodo(v);     // enlaza el primer nodo que se crea en el puntero de la cola
    else
      { pnodo aux = primero;
         while ( aux->siguiente != NULL)
@@ -51,11 +53,11 @@ void lista::InsertarFinal(string v)
       }
 }
 
-void lista::Mostrar()
+void lista::Mostrar(int i)
 {
    nodo *aux;
 
-   aux = primero;
+   aux = Cola[i];   
    while(aux) {
       cout << aux->dato << "-> ";
       aux = aux->siguiente;
@@ -63,25 +65,38 @@ void lista::Mostrar()
    cout << endl;
 }
 
-void lista::lectura(string arch){
+void lista::lectura(string arch, int i){
 
     ifstream archivo;
     string texto;
 
     archivo.open(arch);
-
-    while (!archivo.eof()){  //mientras el archivo no acabe
+    if (archivo.fail())
+        cout << "Lectura del archivo " << i << " fallida" << endl;
+    while (!archivo.eof()){
         getline(archivo,texto);
-        InsertarFinal(texto);
-        Mostrar();
-        cout << texto << endl;
+        InsertarFinal(texto, i);
+        //cout << texto << endl;
     }
+    Mostrar(i);
     archivo.close();
 }
 
+
+
 int main()
-{   lista lista0, lista1, lista2, lista3, lista4;  //una lista para cada archivo
-    string file0 = "operaciones.txt";              //nombre del archivo
-    lista0.lectura(file0);
+{   lista lista0, lista1, lista2, lista3, lista4;
+    string file0 = "operaciones.txt";  //nombre del archivo
+    string file1 = "operaciones1.txt";
+    string file2 = "operaciones2.txt";
+    string file3 = "operaciones3.txt";
+    string file4 = "operaciones4.txt";
+
+    lista0.lectura(file0,0);
+    lista1.lectura(file1,1);
+    lista2.lectura(file2,2);
+    lista3.lectura(file3,3);
+    lista4.lectura(file4,4);
+
 
 }
